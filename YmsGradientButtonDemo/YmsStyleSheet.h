29e73c781b5eec6af0e2d9202c1ac946d198d0da
Charles Choi <charles.choi@yummymelon.com>
2012-01-19 23:42:45 -0800

    * Fixed RGBA() macro * Added ARGBCSS() macro

--- YmsGradientButtonDemo/YmsStyleSheet.h
+++ YmsGradientButtonDemo/YmsStyleSheet.h
@@ -21,8 +21,8 @@
 
 
 #define RGB(r, g, b) [UIColor colorWithRed:r/255.0 green: g/255.0 blue:b/255.0 alpha:1.0]
-#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green: g/255.0 blue:b/255.0 alpha:a]
+#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green: g/255.0 blue:b/255.0 alpha:a/255.0]
 #define RGBCSS(rgb) RGB((double)(rgb >> 16 & 0xff), (double)(rgb >> 8 & 0xff), (double)(rgb & 0xff))
-
+#define ARGBCSS(argb) RGBA((double)(argb >> 16 & 0xff), (double)(argb >> 8 & 0xff), (double)(argb & 0xff), (double)(argb >> 24 & 0xff))
 
 #endif
